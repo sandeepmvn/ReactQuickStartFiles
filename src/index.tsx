46efd14ceab20e3bf6b69a640ad59f1dcee1a06f
Sandeep <sanday1994@gmail.com>
2017-12-14 15:17:49 +0530

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+class MyComponent extends React.Component<{}, {}> {
+    render() {
+        return <div>
+            <h1>Hello World</h1>
+            <p> This is my first React Component</p><span>Build by using ES6</span>
+        </div>
+    }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById('body'));
